69193f75fc2862ad840f55b66d0523d3594d03c9
MinJung Kim <kminss3711@gmail.com>
2023-11-20 10:24:55 +0900

    Feat: 파라미터 누락 시 예외처리 추가
    API 요청시 필요한 파라미터가 누락됐을 경우 예외 처리 추가함

--- src/main/java/com/sparta/todo/advice/GlobalExceptionHandler.java
+++ src/main/java/com/sparta/todo/advice/GlobalExceptionHandler.java
@@ -5,12 +5,14 @@ import com.sparta.todo.exception.ApiException;
 import com.sparta.todo.exception.ErrorCode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -46,7 +48,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     /**
      * [Exception] API 요청 시 '객체' 혹은 '파라미터' 데이터 값이 유효하지 않은 경우
      *
-     * @param ex MethodArgumentNotValidException
+     * @param ex MethodArgumentNotValidException,
      * @return ResponseEntity<ErrorResponse>
      */
     @Override
@@ -65,6 +67,26 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                         .build()
         );
     }
+
+    /**
+     * [Exception] API 요청에 맞는 파라미터를 받지 못한 경우
+     *
+     * @param ex MissingServletRequestParameterException,
+     * @return ResponseEntity<ErrorResponse>
+     */
+    @Override
+    protected ResponseEntity<Object> handleMissingServletRequestParameter(MissingServletRequestParameterException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                ErrorResponse.builder()
+                        .status(INVALID_VALUE.getHttpStatus().value())
+                        .name(HttpStatus.BAD_REQUEST.name())
+                        .message("잘못된 요청입니다." + "누락된 파라미터: " + ex.getParameterName())
+
+                        .build()
+        );
+    }
+
     /**
      * [Exception] API 호출 시 CustomException 으로 정의한 예외가 반환되는 경우
      *
